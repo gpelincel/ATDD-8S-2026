f5a95da1fbffeda794f2ffd5a6131e988ad5df8b
Gabriel Pelincel <gabrielpelincel@gmail.com>
2026-09-08 20:24:53 -0300

    First commit

---
+++ src/main/java/com/gabrielpelincel/attd_project/AttdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielpelincel.attd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielpelincel/attd_project/AttdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpelincel.attd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
